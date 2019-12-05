e8dd3dfa3d8c0536ceb02c5ca3a0983927b49c6c
suyuan <suyuan326@qq.com>
2019-12-05 22:21:56 +0800

    add 1.ts

---
+++ 1.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env ts-node
+//若是有使用过Linux或者Unix的前端开发者，对于Shebang应该不陌生，
+//它是一个符号的名称，#！。这个符号通常在Unix系统的基本中第一行开头中出现，用于指明这个脚本文件的解释程序。
+// /usr/bin/env就是告诉系统可以在PATH目录中查找
+console.log('hello world')
