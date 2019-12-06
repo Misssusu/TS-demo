f7de26ee9a8c1ea20880db4955005a410bb1a0d2
suyuan <suyuan326@qq.com>
2019-12-06 15:22:11 +0800

    实现计算器加法功能

---
+++ calculator/calculator.ts
@@ -0,0 +1,69 @@
+//1.创建0-9数字按钮并显示在页面上
+let calculator: HTMLElement = document.createElement('div');
+let output: HTMLElement = document.createElement('div');
+output.className = 'output';
+output.textContent = '0';
+calculator.className = 'calculator';
+document.body.appendChild(calculator);
+calculator.appendChild(output)
+
+function createBtn(content: string, cotainer: HTMLElement){
+    let btn: HTMLButtonElement = document.createElement('button');
+    btn.textContent = content;
+    cotainer.appendChild(btn);
+}
+
+let arr: Array<Array<string>> = [
+    ['Clear','/'],
+    ['7','8','9','x'],
+    ['4','5','6','-'],
+    ['1','2','3','+'],
+    ['0','.','=']
+]
+arr.forEach((itemList: Array<string>) => {
+    let div = document.createElement('div')
+    itemList.forEach((item: string) => {
+        createBtn(item,div);
+    })
+    calculator.appendChild(div)
+})
+
+let n1: number, n2: number, operate: string, result: number
+calculator.addEventListener('click',(event)=>{
+    if(event.target instanceof HTMLButtonElement){
+        let content: string = event.target.textContent;
+        if('1,2,3,4,5,6,7,8,9,0'.indexOf(content)>=0){
+            if(operate){
+                if(n2) {
+                    n2 = parseInt(n2.toString() + content);
+                }else {
+                    n2 = parseInt(content)
+                }
+            }else {
+                if(n1) {
+                    n1 = parseInt(n1.toString() + content);
+                }else {
+                    n1 = parseInt(content)
+                }
+            }
+        }else if('+,-,*,/'.indexOf(content)>=0){
+            operate = content;
+        }else if('='.indexOf(content)>=0){
+            if(operate === '+'){
+                result = n1 + n2;
+            }else if(operate === '-'){
+                result = n1 - n2;
+            }else if(operate === '*'){
+                result = n1 * n2;
+            }else if(operate === '/'){
+                result = n1 / n2
+            }
+            console.log(result)
+            output.textContent = result.toString()
+        }
+        console.log(n1,operate,n2)
+    }
+
+})
+
+
