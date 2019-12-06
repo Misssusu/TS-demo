755494a14da4a6225085bff4dcc1ce8baeacaf76
suyuan <suyuan326@qq.com>
2019-12-06 16:56:49 +0800

    改用class类实现计算器

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,71 +1,92 @@
-//1.创建0-9数字按钮并显示在页面上
-var calculator = document.createElement('div');
-var output = document.createElement('div');
-output.className = 'output';
-output.textContent = '0';
-calculator.className = 'calculator';
-document.body.appendChild(calculator);
-calculator.appendChild(output);
-function createBtn(content, cotainer) {
-    var btn = document.createElement('button');
-    btn.textContent = content;
-    cotainer.appendChild(btn);
-}
-var arr = [
-    ['Clear', '/'],
-    ['7', '8', '9', 'x'],
-    ['4', '5', '6', '-'],
-    ['1', '2', '3', '+'],
-    ['0', '.', '=']
-];
-arr.forEach(function (itemList) {
-    var div = document.createElement('div');
-    itemList.forEach(function (item) {
-        createBtn(item, div);
-    });
-    calculator.appendChild(div);
-});
-var n1, n2, operate, result;
-calculator.addEventListener('click', function (event) {
-    if (event.target instanceof HTMLButtonElement) {
-        var content = event.target.textContent;
-        if ('1,2,3,4,5,6,7,8,9,0'.indexOf(content) >= 0) {
-            if (operate) {
-                if (n2) {
-                    n2 = parseInt(n2.toString() + content);
-                }
-                else {
-                    n2 = parseInt(content);
-                }
-            }
-            else {
-                if (n1) {
-                    n1 = parseInt(n1.toString() + content);
-                }
-                else {
-                    n1 = parseInt(content);
-                }
-            }
-        }
-        else if ('+,-,*,/'.indexOf(content) >= 0) {
-            operate = content;
-        }
-        else if ('='.indexOf(content) >= 0) {
-            if (operate === '+') {
-                result = n1 + n2;
-            }
-            else if (operate === '-') {
-                result = n1 - n2;
-            }
-            else if (operate === '*') {
-                result = n1 * n2;
-            }
-            else if (operate === '/') {
-                result = n1 / n2;
-            }
-            console.log(result);
-            output.textContent = result.toString();
-        }
-        console.log(n1, operate, n2);
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+        this.createCalculator();
+        this.createOutput();
+        this.createBtn();
+        this.bindEvents();
     }
-});
+    Calculator.prototype.createCalculator = function () {
+        this.calculator = document.createElement('div');
+        this.calculator.className = 'calculator';
+        document.body.appendChild(this.calculator);
+    };
+    Calculator.prototype.createOutput = function () {
+        this.output = document.createElement('div');
+        this.output.className = 'output';
+        this.output.textContent = '0';
+        this.calculator.appendChild(this.output);
+    };
+    Calculator.prototype.createBtn = function () {
+        var _this = this;
+        var arr = [
+            ['Clear', '/'],
+            ['7', '8', '9', 'x'],
+            ['4', '5', '6', '-'],
+            ['1', '2', '3', '+'],
+            ['0', '.', '=']
+        ];
+        arr.forEach(function (itemList) {
+            var div = document.createElement('div');
+            itemList.forEach(function (item) {
+                createBtn(item, div);
+            });
+            _this.calculator.appendChild(div);
+        });
+        function createBtn(content, cotainer) {
+            var btn = document.createElement('button');
+            btn.textContent = content;
+            cotainer.appendChild(btn);
+        }
+    };
+    Calculator.prototype.bindEvents = function () {
+        var _this = this;
+        this.calculator.addEventListener('click', function (event) {
+            if (event.target instanceof HTMLButtonElement) {
+                var content = event.target.textContent;
+                //判断点击的按钮类型
+                if ('1,2,3,4,5,6,7,8,9,0'.indexOf(content) >= 0) {
+                    if (_this.operate) {
+                        if (_this.n2) {
+                            _this.n2 = parseInt(_this.n2.toString() + content);
+                        }
+                        else {
+                            _this.n2 = parseInt(content);
+                        }
+                        _this.output.textContent = _this.n2.toString();
+                    }
+                    else {
+                        if (_this.n1) {
+                            _this.n1 = parseInt(_this.n1.toString() + content);
+                        }
+                        else {
+                            _this.n1 = parseInt(content);
+                        }
+                        _this.output.textContent = _this.n1.toString();
+                    }
+                }
+                else if ('+,-,*,/'.indexOf(content) >= 0) {
+                    _this.operate = content;
+                }
+                else if ('='.indexOf(content) >= 0) {
+                    if (_this.operate === '+') {
+                        _this.result = _this.n1 + _this.n2;
+                    }
+                    else if (_this.operate === '-') {
+                        _this.result = _this.n1 - _this.n2;
+                    }
+                    else if (_this.operate === '*') {
+                        _this.result = _this.n1 * _this.n2;
+                    }
+                    else if (_this.operate === '/') {
+                        _this.result = _this.n1 / _this.n2;
+                    }
+                    console.log(_this.result);
+                    _this.output.textContent = _this.result.toString();
+                }
+                console.log(_this.n1, _this.operate, _this.n2);
+            }
+        });
+    };
+    return Calculator;
+}());
+new Calculator();
--- calculator/calculator.ts
+++ calculator/calculator.ts
@@ -1,69 +1,92 @@
-//1.创建0-9数字按钮并显示在页面上
-let calculator: HTMLElement = document.createElement('div');
-let output: HTMLElement = document.createElement('div');
-output.className = 'output';
-output.textContent = '0';
-calculator.className = 'calculator';
-document.body.appendChild(calculator);
-calculator.appendChild(output)
-
-function createBtn(content: string, cotainer: HTMLElement){
-    let btn: HTMLButtonElement = document.createElement('button');
-    btn.textContent = content;
-    cotainer.appendChild(btn);
-}
-
-let arr: Array<Array<string>> = [
-    ['Clear','/'],
-    ['7','8','9','x'],
-    ['4','5','6','-'],
-    ['1','2','3','+'],
-    ['0','.','=']
-]
-arr.forEach((itemList: Array<string>) => {
-    let div = document.createElement('div')
-    itemList.forEach((item: string) => {
-        createBtn(item,div);
-    })
-    calculator.appendChild(div)
-})
-
-let n1: number, n2: number, operate: string, result: number
-calculator.addEventListener('click',(event)=>{
-    if(event.target instanceof HTMLButtonElement){
-        let content: string = event.target.textContent;
-        if('1,2,3,4,5,6,7,8,9,0'.indexOf(content)>=0){
-            if(operate){
-                if(n2) {
-                    n2 = parseInt(n2.toString() + content);
-                }else {
-                    n2 = parseInt(content)
-                }
-            }else {
-                if(n1) {
-                    n1 = parseInt(n1.toString() + content);
-                }else {
-                    n1 = parseInt(content)
-                }
-            }
-        }else if('+,-,*,/'.indexOf(content)>=0){
-            operate = content;
-        }else if('='.indexOf(content)>=0){
-            if(operate === '+'){
-                result = n1 + n2;
-            }else if(operate === '-'){
-                result = n1 - n2;
-            }else if(operate === '*'){
-                result = n1 * n2;
-            }else if(operate === '/'){
-                result = n1 / n2
-            }
-            console.log(result)
-            output.textContent = result.toString()
-        }
-        console.log(n1,operate,n2)
+class Calculator{
+    public calculator: HTMLElement;
+    public output: HTMLElement;
+    public n1: number;
+    public n2: number;
+    public operate: string;
+    public result: number;
+    constructor(){
+        this.createCalculator();
+        this.createOutput();
+        this.createBtn();
+        this.bindEvents()
     }
+    createCalculator(){
+        this.calculator = document.createElement('div');
+        this.calculator.className = 'calculator';
+        document.body.appendChild(this.calculator);
+    }
+    createOutput(){
+        this.output = document.createElement('div');
+        this.output.className = 'output';
+        this.output.textContent = '0';
+        this.calculator.appendChild(this.output)
+    }
+    createBtn(){
+        let arr: Array<Array<string>> = [
+            ['Clear','/'],
+            ['7','8','9','x'],
+            ['4','5','6','-'],
+            ['1','2','3','+'],
+            ['0','.','=']
+        ];
+        arr.forEach((itemList: Array<string>) => {
+            let div = document.createElement('div');
+            itemList.forEach((item: string) => {
+                createBtn(item,div);
+            });
+            this.calculator.appendChild(div)
+        });
+        function createBtn(content: string, cotainer: HTMLElement){
+            let btn: HTMLButtonElement = document.createElement('button');
+            btn.textContent = content;
+            cotainer.appendChild(btn);
+        }
+    }
+    bindEvents(){
+        this.calculator.addEventListener('click',(event)=>{
+            if(event.target instanceof HTMLButtonElement){
+                let content: string = event.target.textContent;
+                //判断点击的按钮类型
+                if('1,2,3,4,5,6,7,8,9,0'.indexOf(content)>=0){
+                    if(this.operate){
+                        if(this.n2) {
+                            this.n2 = parseInt(this.n2.toString() + content);
+                        }else {
+                            this.n2 = parseInt(content)
+                        }
+                        this.output.textContent = this.n2.toString()
 
-})
+                    }else {
+                        if(this.n1) {
+                            this.n1 = parseInt(this.n1.toString() + content);
+                        }else {
+                            this.n1 = parseInt(content)
+                        }
+                        this.output.textContent = this.n1.toString()
+
+                    }
+                }else if('+,-,*,/'.indexOf(content)>=0){
+                    this.operate = content;
+                }else if('='.indexOf(content)>=0){
+                    if(this.operate === '+'){
+                        this.result = this.n1 + this.n2;
+                    }else if(this.operate === '-'){
+                        this.result = this.n1 - this.n2;
+                    }else if(this.operate === '*'){
+                        this.result = this.n1 * this.n2;
+                    }else if(this.operate === '/'){
+                        this.result = this.n1 / this.n2
+                    }
+                    console.log(this.result);
+                    this.output.textContent = this.result.toString()
+                }
+                console.log(this.n1,this.operate,this.n2)
+            }
+        })
+    }
+}
+new Calculator()
+
 
 
